103479dec42a2073aba09c5ae5765c044e2db28a
Carlos <16615567+car13mesquita@users.noreply.github.com>
2021-11-16 20:30:54 -0500

    Update for csv writeback

--- api/companies.py
+++ api/companies.py
@@ -3,13 +3,15 @@ import pandas as pd
 
 companies = Blueprint('companies', __name__, url_prefix="/companies")
 
+companies_filepath = "./dataset/file1.csv"
 
 @companies.route("/", methods=["POST", "GET"])
 def post():
-    df = pd.read_csv("./dataset/file1.csv")
+    df = pd.read_csv(companies_filepath)
     if request.method == "POST":
-        data = df.append({'_id': "new_ID", 'industries': "new_industry"}, ignore_index=True).to_dict()
-        return data, 200
+        data = df.append({'_id': "new_ID", 'industries': "new_industry"}, ignore_index=True)
+        data.to_csv(companies_filepath, encoding='utf-8')
+        return 'complete', 200
     else:
         data = df.to_dict()
         return data, 200
@@ -17,13 +19,14 @@ def post():
 
 @companies.route("/<id>", methods=["PUT", "DELETE", "GET"])
 def put_delete(id):
-    df = pd.read_csv("./dataset/file1.csv")
+    df = pd.read_csv(companies_filepath)
     if request.method == "PUT":
         # add stuff here
         return 200
     elif request.method == "DELETE":
-        data = df.drop(index=df.index[df['_id'] == id], axis=0).to_dict()
-        return data, 200
+        data = df.drop(index=df.index[df['_id'] == id], axis=0)
+        data.to_csv(companies_filepath, encoding='utf-8')
+        return 'complete', 200
     else:
         data = df.loc[df['_id'] == id].to_dict()
         return data, 200
